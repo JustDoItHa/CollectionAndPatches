74d86b819ff85967a4249a05217df05ecd6808f3
elvis <elvis.2020c@outlook.com>
2023-02-10 20:23:19 +0800

    remove smart mini sign for big_box and medal krampus chest

--- scripts/smart_minisign_main.lua
+++ scripts/smart_minisign_main.lua
@@ -31,7 +31,7 @@ AddClassPostConstruct( "components/inventoryitem_replica", function(self, inst)
 		return self._huastrings ~= nil and
 			self._huastrings ~= "" and
 			self._huastrings or
-			self:GetAtlas()	
+			self:GetAtlas()
 	end
 end)
 
@@ -60,9 +60,14 @@ end)
 
 local function draw(inst)
 
-    if not GLOBAL.TheWorld.ismastersim then
-        return inst
-    end
+	if not GLOBAL.TheWorld.ismastersim then
+		return inst
+	end
+
+	if inst.prefab == "_big_box" or inst.prefab == "medal_krampus_chest" then
+		return inst
+	end
+
 	inst:AddComponent("smart_minisign")
 end
 
@@ -81,28 +86,28 @@ end
 --读取所有已加载的mod
 local enabledmods = GLOBAL.ModManager.enabledmods
 
-local thisname = env.modname 
+local thisname = env.modname
 --hook一下加载prefabs 加载完prefabs在检索数据
-local oldRegisterPrefabs = GLOBAL.ModManager.RegisterPrefabs 
+local oldRegisterPrefabs = GLOBAL.ModManager.RegisterPrefabs
 
 GLOBAL.ModManager.RegisterPrefabs = function(self)
 	oldRegisterPrefabs(self)
-	
+
 	for i,modname in ipairs(enabledmods) do
 		local mod = GLOBAL.ModManager:GetMod(modname)
-		
+
 		--检索 modmain里注册的资源
-		if mod.Assets then 
+		if mod.Assets then
 			local modatlas = {}
 			local modatlas_build = {}
 			--检索所有的贴图
 			for k,v in ipairs (mod.Assets) do
-				if v.type == "ATLAS" then 
+				if v.type == "ATLAS" then
 					table.insert(modatlas,v.file)
-				elseif v.type == "ATLAS_BUILD" then 
+				elseif v.type == "ATLAS_BUILD" then
 					table.insert(modatlas_build,v.file)
 				end
-				
+
 			end
 			--判断是否有对应的ATLAS_BUILD
 			for k,v in ipairs(modatlas) do
@@ -121,7 +126,7 @@ GLOBAL.ModManager.RegisterPrefabs = function(self)
 				end
 			end
 		end
-		
+
 		--检索 prefabs 里注册的资源
 		if mod.Prefabs then
 			for n,prefab in pairs(mod.Prefabs) do
@@ -130,9 +135,9 @@ GLOBAL.ModManager.RegisterPrefabs = function(self)
 				--检索所有的贴图
 				if prefab.assets then
 					for k,v in pairs (prefab.assets) do
-						if v.type == "ATLAS" then 
+						if v.type == "ATLAS" then
 							table.insert(modatlas,v.file)
-						elseif v.type == "ATLAS_BUILD" then 
+						elseif v.type == "ATLAS_BUILD" then
 							table.insert(modatlas_build,v.file)
 						end
 					end
@@ -159,7 +164,7 @@ GLOBAL.ModManager.RegisterPrefabs = function(self)
 	GLOBAL.RegisterPrefabs(GLOBAL.Prefab("MOD_SMARTSIGNOTHER",nil,Assets,nil,true))
 	GLOBAL.TheSim:LoadPrefabs({"MOD_SMARTSIGNOTHER"})
 	table.insert(self.loadedprefabs,"MOD_SMARTSIGNOTHER")
-	
+
 end
 
 GLOBAL.TUNING.SMART_SIGN_DRAW_ENABLE = true
