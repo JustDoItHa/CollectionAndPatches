9b645df764284723b3e004a93fd3251bdc905f0f
elvis <elvis.2020c@outlook.com>
2023-02-26 10:11:42 +0800

    limit ab black gold

--- modinfo.lua
+++ modinfo.lua
@@ -24,7 +24,7 @@ description = [[
 ----------------------------------------------------------------------
 
 author = "EL"
-version = "8.22.2.0"
+version = "8.23.2.0"
 
 folder_name = folder_name or "Collection And Patches[合集和补丁]"
 if not folder_name:find("workshop-") then
