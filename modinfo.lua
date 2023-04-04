ef2d0721d132bae4470e2845c6e8d2bd1bb05a44
null <you@example.com>
2023-04-04 10:40:51 +0800

    clean fix

--- modinfo.lua
+++ modinfo.lua
@@ -24,7 +24,7 @@ description = [[
 ----------------------------------------------------------------------
 
 author = "EL"
-version = "9.1.0.1"
+version = "9.1.0.2"
 
 folder_name = folder_name or "Collection And Patches[合集和补丁]"
 if not folder_name:find("workshop-") then
