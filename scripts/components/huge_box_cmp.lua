6887acbba0026f6efd4b0cc3348d224f832ac241
null <you@example.com>
2023-04-07 15:34:53 +0800

    nil

--- scripts/components/huge_box_cmp.lua
+++
@@ -1,8 +0,0 @@
----
---- @author zsh in 2023/1/26 17:18
----
-
-local T = Class(function(self, inst)
-    self.inst = inst;
-end)
-return T;
--- scripts/yuanzi_patches.lua
+++
@@ -1,8 +0,0 @@
-
-if GetModConfigData("divinetree_no_health") then
-    AddPrefabPostInit("divinetree",function(inst)                -- 神树，真无敌
-        if inst.components.health then
-            inst:RemoveComponent("health")
-        end
-    end)
-end
