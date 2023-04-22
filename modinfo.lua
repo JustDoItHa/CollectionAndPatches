6b87442ef578b1ebc220204d2fef5d5d3fd71610
elvis <441664842@qq.com>
2023-04-23 04:54:11 +0800

    optimize

--- modinfo.lua
+++ modinfo.lua
@@ -24,7 +24,7 @@ description = [[
 ----------------------------------------------------------------------
 
 author = "EL"
-version = "9.7.0.5"
+version = "9.7.0.6"
 
 folder_name = folder_name or "Collection And Patches[合集和补丁]"
 if not folder_name:find("workshop-") then
