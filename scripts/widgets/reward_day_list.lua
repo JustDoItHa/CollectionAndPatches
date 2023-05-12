56cab9698ef97f627fcb29cb8c8a05cc7f815b86
elvis <441664842@qq.com>
2023-05-12 10:19:22 +0800

    optimize

--- scripts/widgets/reward_day_list.lua
+++ scripts/widgets/reward_day_list.lua
@@ -1,22 +1,23 @@
 rewardlist = {
-    {
-        day = 2,--奖励天数
-        tips="肉串一串",
-        reward = {
-            {code = "kabobs", number = 1}, --奖励物品、数量
-        }
-    },
-    {
-        day = 5,--奖励天数
-        tips="牛屎一坨",--恭喜玩家“xxxx”生存xxxx天，奖励牛屎一坨
-        reward = {
-            {code = "poop", number = 10}, --奖励物品、数量
-        }
-    },
+    --{
+    --    day = 2,--奖励天数
+    --    tips="肉串一串",
+    --    reward = {
+    --        {code = "kabobs", number = 1}, --奖励物品、数量
+    --    }
+    --},
+    --{
+    --    day = 5,--奖励天数
+    --    tips="牛屎一坨",--恭喜玩家“xxxx”生存xxxx天，奖励牛屎一坨
+    --    reward = {
+    --        {code = "poop", number = 10}, --奖励物品、数量
+    --    }
+    --},
     {
         day = 10,
         tips="物资套件一套",
         reward = {
+            {code = "poop", number = 10},
             {code = "cutgrass", number = 10},
             {code = "twigs", number = 10},
             {code = "log", number = 10},
@@ -30,8 +31,8 @@ rewardlist = {
         }
     },
     {
-        day = 20,--奖励天数
-        tips="宝石礼包-史诗",
+        day = 30,--奖励天数
+        tips="满月宝石礼包-史诗",
         reward = {
             { code = "redgem", number = 10 }, --红宝石
             { code = "bluegem", number = 10 }, --蓝宝石
@@ -39,8 +40,8 @@ rewardlist = {
         }
     },
     {
-        day = 50,--奖励天数
-        tips="宝石礼包-传说",
+        day = 70,--奖励天数
+        tips="一岁宝石礼包-传说",
         reward = {
             { code = "greengem", number = 10 }, --绿宝石
             { code = "orangegem", number = 10 }, --橙宝石
@@ -49,11 +50,15 @@ rewardlist = {
     },
     {
         day = 100,--奖励天数
-        tips="宝石礼包-传说",
+        tips="百天宝石礼包-金色传说",
         reward = {
+            { code = "redgem", number = 10 }, --红宝石
+            { code = "bluegem", number = 10 }, --蓝宝石
+            { code = "purplegem", number = 10 }, --紫宝石
             { code = "greengem", number = 10 }, --绿宝石
             { code = "orangegem", number = 10 }, --橙宝石
             { code = "yellowgem", number = 10 }, --黄宝石
+            { code = "opalpreciousgem", number = 10 }, --彩虹宝石
         }
     },
 }
