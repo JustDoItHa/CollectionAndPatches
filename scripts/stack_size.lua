8f9ac1b54d468872ee6d05dc7fa03b6bfb9af882
elvischeng <elvischeng@outlook.com>
2024-03-28 12:06:32 +0800

    fix

--- scripts/stack_size.lua
+++ scripts/stack_size.lua
@@ -36,7 +36,18 @@ if stack_size1 ~= 20 then
     GLOBAL.TUNING.WORTOX_MAX_SOULS = stack_size1
 end
 
+
 if stack_size ~= 20 or stack_size1 ~= 20 then
+    local function OnStackSizeDirty(inst)
+        local self = inst.replica.stackable
+        if not self then
+            return --stackable removed?
+        end
+
+        self:ClearPreviewStackSize()
+        inst:PushEvent("inventoryitem_stacksizedirty")
+    end
+
     local mod_stackable_replica = GLOBAL.require("components/stackable_replica")
     mod_stackable_replica._ctor = function(self, inst)
         self.inst = inst
@@ -46,6 +57,9 @@ if stack_size ~= 20 or stack_size1 ~= 20 then
         self._stacksizeupper = GLOBAL.net_shortint(inst.GUID, "stackable._stacksizeupper", "stacksizedirty")
         self._ignoremaxsize = GLOBAL.net_bool(inst.GUID, "stackable._ignoremaxsize")
         self._maxsize = GLOBAL.net_shortint(inst.GUID, "stackable._maxsize")
+        if not GLOBAL.TheWorld.ismastersim then
+            inst:ListenForEvent("stacksizedirty", OnStackSizeDirty)
+        end
     end
 end
 
