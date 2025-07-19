699e4c67a470d283f7d0fc325e48c82b378b46d9
elvischeng <elvischeng@outlook.com>
2025-07-20 03:38:07 +0800

    optimize

--- modinfo.lua
+++ modinfo.lua
@@ -26,7 +26,7 @@ description = [[
 ----------------------------------------------------------------------
 
 author = "EL"
-version = "14.9.9.0"
+version = "14.9.10.0"
 
 folder_name = folder_name or "Collection And Patches[合集和补丁]"
 if not folder_name:find("workshop-") then
