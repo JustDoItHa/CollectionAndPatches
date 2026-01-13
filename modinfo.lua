9e03d67881975f6ea60bfac33c2ae0aba1441dae
Mo-yuer <86790358+Mo-yuer@users.noreply.github.com>
2026-01-13 14:58:51 +0800

    Add files via upload
    更新版本号

--- modinfo.lua
+++ modinfo.lua
@@ -26,7 +26,7 @@ description = [[
 ----------------------------------------------------------------------
 
 author = "EL"
-version = "14.10.1.0"
+version = "14.10.1.1"
 
 folder_name = folder_name or "Collection And Patches[合集和补丁]"
 if not folder_name:find("workshop-") then
